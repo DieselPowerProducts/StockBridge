8b0ad590f01a52cbb144cf4e36d6c5aab7c582b5
CadeDPP <cade@dieselpowerproducts.com>
2026-04-24 08:29:39 -0700

    Pass Shopify lookup tie-breakers through controller

--- server/controllers/shopify.controller.js
+++ server/controllers/shopify.controller.js
@@ -3,8 +3,10 @@ const shopifyService = require("../services/shopify.service");
 async function resolveOrder(req, res, next) {
   try {
     const order = await shopifyService.resolveOrder({
+      createdAt: req.body.createdAt,
       orderNumber: req.body.orderNumber,
-      customerEmail: req.body.customerEmail
+      customerEmail: req.body.customerEmail,
+      skus: req.body.skus
     });
 
     res.send({ order });
